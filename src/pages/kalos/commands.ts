610ab2b54151bb66bd25b1e22e70c16b2749f4d7
elykbv <elii.yakubov@gmail.com>
2024-08-24 07:30:26 -0700

    feat(Commands): basic commands with zustand callbacks

--- src/pages/kalos/commands.ts
+++ src/pages/kalos/commands.ts
@@ -1,3 +1,28 @@
-export const commands = [
+import useSystemStore from "./stores/systemStore";
 
-]
+export const commands = [
+  {
+    command: "Timer Left System",
+    callback: () => useSystemStore.getState().turnOnLeft(),
+  },
+  {
+    command: "Timer Right System",
+    callback: () => useSystemStore.getState().turnOnRight(),
+  },
+  {
+    command: "Timer Top System",
+    callback: () => useSystemStore.getState().turnOnTop(),
+  },
+  {
+    command: "Timer Bottom System",
+    callback: () => useSystemStore.getState().turnOnBottom(),
+  },
+  {
+    command: "Timer Left Cleansed",
+    callback: () => useSystemStore.getState().cleanseLeft(),
+  },
+  {
+    command: "Timer Right Cleansed",
+    callback: () => useSystemStore.getState().cleanseRight(),
+  },
+];
